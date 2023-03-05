6421c683acba4dc7fab19ae4fe8ffd42a3750302
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-03-05 09:50:14 -0500

    add env variable for db ref

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //MongoDB Atlas deployment ->
-  "URI": "mongodb+srv://admin:kuHGFxDhWdmKHWQ9@cluster0.t5jzlyx.mongodb.net/?retryWrites=true&w=majority"
+  "URI": env.process.DB_URI
 };
