e98a864d96cdf8820e866a8a472616691067ef81
Kugavathanan Yohanathan <kugajpx2012@gmail.com>
2023-03-05 19:47:36 -0500

    updated db uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //MongoDB Atlas deployment ->
-  "URI": process.env.DB_URI || "mongodb+srv://admin:kuHGFxDhWdmKHWQ9@cluster0.t5jzlyx.mongodb.net/?retryWrites=true&w=majority"
+  "URI":  "mongodb+srv://admin:kuHGFxDhWdmKHWQ9@cluster0.t5jzlyx.mongodb.net/?retryWrites=true&w=majority"
 };
